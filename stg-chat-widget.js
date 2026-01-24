79e54347312cc738c02d7fa8d4c3d9e85a79f2b7
manuelmedinablackorbit <manuelmedina@blackorbitai.com>
2026-01-24 08:30:35 +0100

    Update stg-chat-widget.js

--- stg-chat-widget.js
+++ stg-chat-widget.js
@@ -473,9 +473,6 @@
           <div class="bo-input-container">
             <div class="bo-input-wrapper">
               <input type="text" class="bo-input" id="bo-input" placeholder="${CONFIG.inputPlaceholder}">
-              <svg class="bo-attach-icon" fill="none" stroke="#5E7690" viewBox="0 0 24 24" stroke-width="1.33">
-                <path d="M21.44 11.05l-9.19 9.19a6 6 0 01-8.49-8.49l9.19-9.19a4 4 0 015.66 5.66l-9.2 9.19a2 2 0 01-2.83-2.83l8.49-8.48"/>
-              </svg>
             </div>
             <button class="bo-send-btn" id="bo-send">
               <svg width="16" height="16" fill="none" stroke="white" viewBox="0 0 24 24" stroke-width="1.33">
